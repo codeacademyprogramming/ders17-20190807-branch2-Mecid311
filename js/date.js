b1fd51bb5f17c6576d133d80e58aa37e784ccdbd
akamran <akamran@CAPR0>
2019-08-07 21:30:22 +0400

    Start + Stop

--- js/date.js
+++ js/date.js
@@ -20,10 +20,10 @@
 
 
 
-function showDateTime() {
-    var dateTime = new Date();
+function showDateTime(diff = 0) {
+    let dateTime = new Date(Date.now() - diff);
     // console.log(typeof dateTime.getMonth());
-    var dateTimeString = dateTime.getDate().padLeft('00') + '.'
+    let dateTimeString = dateTime.getDate().padLeft('00') + '.'
         + (dateTime.getMonth() + 1).padLeft('00') + '.'
         + dateTime.getFullYear().padLeft('0000') + ' '
         + dateTime.getHours().padLeft('00') + ':'
@@ -33,24 +33,47 @@ function showDateTime() {
 }
 
 
-function timeInfinitive() {
-    showDateTime();
-    var pid = setInterval(showDateTime, 1000);
+function timeInfinitive(diff = 0/*optional parameter */) {
+    // if(diff== undefined)
+    // diff=0;
+    console.log(diff);
+    showDateTime(diff);
+    let pid = setInterval(showDateTime, 1000,diff);
 
-    document.getElementById('btnStop').setAttribute('pid',pid);
+    document.getElementById('btnStop').setAttribute('pid', pid);
 }
 // '7'.padLeft('00');
 
 Number.prototype.padLeft = function (template) {
-// console.log(typeof this,this);
+    // console.log(typeof this,this);
     return template.substring(0, template.length - this.toString().length)
         + this.toString();
 }
 
 
-function timeStop(el)
-{
+function timeStop(el) {
     console.log(el.getAttribute('pid'));
     clearInterval(parseInt(el.getAttribute('pid')));
+}
+
+//07.08.2019 21:03:22
+function timeStart() {
+    let oldDateTimeString = document.getElementById('output').innerHTML;
+    let oldDateTimeArr = oldDateTimeString.split(' ');
+    let year, month, day;
+    let oldDateArr = oldDateTimeArr[0].split('.');
+    year = parseInt(oldDateArr[2]);
+    month = parseInt(oldDateArr[1]) - 1;
+    day = parseInt(oldDateArr[0]);
+
+    let hour, minute, second;
+    let oldTimeArr = oldDateTimeArr[1].split(':');
+    hour = parseInt(oldTimeArr[0]);
+    minute = parseInt(oldTimeArr[1]);
+    second = parseInt(oldTimeArr[2]);
+
+    let oldDateTime = new Date(year, month, day, hour, minute, second);
+    // console.log(Date.now() - oldDateTime.getTime());
+    timeInfinitive(Date.now() - oldDateTime.getTime());
 }
 
